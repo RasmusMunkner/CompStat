4b08044fd5644c796ae81455b14d1a8c47f20f29
RVM PC <RVMunknerDevelopment@gmail.com>
2023-09-27 14:35:25 +0200

    Implemented a much faster approximation to the poisson_prior density.

--- R/poisson_density.R
+++ R/poisson_density.R
@@ -1,5 +1,35 @@
 data("poisson")
 xz_poisson_prior <- sum(poisson$x * poisson$z)
+x_poisson_mean <- mean(poisson$x)
+
+vandermonde <- function(y, n){
+  outer(y, 0:n, `^`) %>% t()
+}
+
+polycoef <- function(n){
+  0:n %>%
+    purrr::map_dbl(.f=function(n){
+      sum((poisson$x - mean(poisson$x))^n)
+    }) / factorial(0:n)
+}
+
+#' Approximation of the poisson prior density
+#'
+#' @param y The points to evaluate the density in.
+#' @param maxpow The order of approximation. Should be at least 2.
+#' The higher the order, the more accurate (and slower) the approximation.
+#'
+#' @return The approximate density evaluated at y.
+#' @export
+#'
+#' @examples
+#' poisson_prior_log_f_approx(seq(0.001, 1, 0.001))
+poisson_prior_log_f_approx <- function(y, maxpow = 4){
+  term1 <- y*xz_poisson_prior
+  exp_approx <- colSums(vandermonde(y, maxpow) * polycoef(maxpow))
+  term2 <- exp(y*x_poisson_mean) * exp_approx
+  term1 - term2
+}
 
 #' Log-density for the poisson prior (Example Distribution)
 #'
@@ -10,6 +40,7 @@ xz_poisson_prior <- sum(poisson$x * poisson$z)
 #'
 #' @examples
 #' poisson_prior_log_f(1)
+#' seq(1e-4, 1, 1e-4) %>% plot(., poisson_prior_log_f(.), type="l")
 poisson_prior_log_f <- function(y){
   term1 <- y*xz_poisson_prior
   term2 <- rowSums(exp(outer(y, poisson$x)))
@@ -118,7 +149,10 @@ get_rv <- function(key = "n"){
                             f_prime = function(z) -dgamma(z,2) + exp(-z)
                             ),
          "p"=RandomVariable(log_f = poisson_prior_log_f,
-                            log_f_prime = poisson_prior_log_f_prime)
+                            log_f_prime = poisson_prior_log_f_prime),
+         "a"=RandomVariable(log_f = poisson_prior_log_f_approx,
+                            log_f_prime = poisson_prior_log_f_prime
+                            )
          )
 }
 
